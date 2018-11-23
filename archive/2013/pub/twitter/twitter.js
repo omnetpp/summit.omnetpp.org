fc0f29eacfbe29f50be9d91187b2edecf22fb1a5
Rudolf Hornig <rudi@omnetpp.org>
2018-11-23 16:37:53 +0100

    initial commit. moved the website from the private server to github.

---
+++ archive/2013/pub/twitter/twitter.js
@@ -0,0 +1,40 @@
+function twitterCallback2(twitters) {
+  var statusHTML = [];
+  for (var i=0; i<twitters.length; i++){
+    var username = twitters[i].user.screen_name;
+    var status = twitters[i].text.replace(/((https?|s?ftp|ssh)\:\/\/[^"\s\<\>]*[^.,;'">\:\s\<\>\)\]\!])/g, function(url) {
+      return '<a href="'+url+'">'+url+'</a>';
+    }).replace(/\B@([_a-z0-9]+)/ig, function(reply) {
+      return  reply.charAt(0)+'<a href="http://twitter.com/'+reply.substring(1)+'">'+reply.substring(1)+'</a>';
+    });
+    statusHTML.push('<div id="twittermessages"><li><span class="twittermessages_status" style="">'+status+'</span> <a class="twittermessages_relativeTime" href="http://twitter.com/'+username+'/status/'+twitters[i].id_str+'">'+relative_time(twitters[i].created_at)+'</a></li></div>');
+  }
+  document.getElementById('twitter_update_list').innerHTML = statusHTML.join('');
+}
+
+function relative_time(time_value) {
+  var values = time_value.split(" ");
+  time_value = values[1] + " " + values[2] + ", " + values[5] + " " + values[3];
+  var parsed_date = Date.parse(time_value);
+  var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+  var delta = parseInt((relative_to.getTime() - parsed_date) / 1000);
+  delta = delta + (relative_to.getTimezoneOffset() * 60);
+
+  if (delta < 60) {
+    return 'just now';
+  } else if(delta < 120) {
+    return 'about a minute ago';
+  } else if(delta < (60*60)) {
+    return (parseInt(delta / 60)).toString() + ' minutes ago';
+  } else if(delta < (120*60)) {
+    return 'about an hour ago';
+  } else if(delta < (24*60*60)) {
+    return 'about ' + (parseInt(delta / 3600)).toString() + ' hours ago';
+  } else if(delta < (48*60*60)) {
+    return 'one day ago';
+  } else {
+    var d1 = new Date();
+    d1.setTime(parsed_date);
+    return d1.getDate() + '.' + (d1.getMonth()+1) + '.' + d1.getFullYear();
+  }
+}
