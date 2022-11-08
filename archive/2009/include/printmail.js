cff4de08dd9db29907db845e3b422c20064c1473
Rudolf Hornig <rudi@omnetpp.org>
2022-11-08 13:57:26 +0100

    moved older site versions to the top level

--- archive/2009/include/printmail.js
+++
@@ -1,9 +0,0 @@
-function printMail(publicname, user, domain, extension)
-{
-  var at='@';
-  var dot='.';
-  var camou1='ma';
-  var camou2='il';
-  var camou3='to:';
-  document.write('<a href="'+camou1+camou2+camou3+user+at+domain+dot+extension+'">'+publicname+'</a>');
-}
